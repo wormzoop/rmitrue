4b86e720881fae8b000393ca85c9bb16d4fec8cc
wormzoop <446706489@qq.com>
2018-01-06 11:12:02 +0800

    1

--- src/com/zoop/rmi/main/RmiClient.java
+++ src/com/zoop/rmi/main/RmiClient.java
@@ -2,4 +2,8 @@ package com.zoop.rmi.main;
 
 public class RmiClient {
 
+	public static Object invoke(String naming) {
+		return null;
+	}
+	
 }
--- src/com/zoop/rmi/main/RmiServer.java
+++ src/com/zoop/rmi/main/RmiServer.java
@@ -2,4 +2,9 @@ package com.zoop.rmi.main;
 
 public class RmiServer {
 
+	//发布
+	public static void binding(String naming) {
+		
+	}
+	
 }
